75cbc59ee1a9995869ea4da719071b71fe14b0d5
Azhan <azhaniqbal45@gmail.com>
2024-08-12 19:46:15 +0500

    mainscreen chagning

--- src/Components/Main Pages/Mainscreen.jsx
+++ src/Components/Main Pages/Mainscreen.jsx
@@ -8,10 +8,10 @@ const Mainscreen = () => {
   return (
    <section className='pt-[15%] bg-black pb-[10%]'>
     <Wrapper>
-    <div class="grid grid-cols-2 gap-4  ">
+    <div className="grid grid-cols-2 gap-4  ">
     <div className='sm:w-full lg:w-[50%]'>
         <GiScissors className='text-[70px] text-red-500'/>
-        <h1 className='text-[60px] text-white mb-4 font-bold '>WE'RE THE LAST OF BREED</h1>
+        <h1 className='text-[60px] text-white mb-4 font-bold '>WE&apos; THE LAST OF BREED</h1>
         <p className='text-white mb-5 text-[24px]'>Achieve Your Dream Style</p>
         <Button text={"Book Now"}/>
     </div>
